4e8cecb4c2885fcc9f979141e4ac3708a8911d70
Alex Macon <alexdmacon@gmail.com>
2021-10-16 16:41:10 -0500

    wrote HTML routes

---
+++ routes/html.js
@@ -0,0 +1,15 @@
+
+
+const htmlRoute = require('express').Router();
+
+const path = require('path');
+
+htmlRoute.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'))
+});
+
+htmlRoute.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+});
+
+module.exports = htmlRoute;
