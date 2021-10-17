8d1f8434bcf8bd2f4506784fa845b7c52bba1cbe
Alex Macon <alexdmacon@gmail.com>
2021-10-17 10:08:36 -0500

    API get route works, post route does not

--- routes/api.js
+++ routes/api.js
@@ -6,11 +6,11 @@ const { v4: uuidv4 } = require("uuid");
 const { readFromFile, writeToFile, readAndAppend } = require('./store')
 
 
-apiRoute.get("/api/notes", (req, res) => {
-    readFromFile('../db/db.json').then((notes) => res.json(JSON.parse(notes)))
+apiRoute.get("/notes", (req, res) => {
+    readFromFile(path.join(__dirname, '../db/db.json')).then((notes) => res.json(JSON.parse(notes)))
 });
 
-apiRoute.post("/api/notes", (req, res) => {
+apiRoute.post("/notes", (req, res) => {
     console.log(req.body);
 
     const { title, text } = req.body;
@@ -22,9 +22,9 @@ apiRoute.post("/api/notes", (req, res) => {
     }
 
     readAndAppend(newNote, '../db/db.json').then(notes => [...notes, newNote])
-    .then(allNotes => writeToFile(allNotes))
+/*     .then(allNotes => writeToFile(allNotes))
     .then(() => newNote)
-    res.json('New note added');
+    res.json('New note added'); */
 });
 
 module.exports = apiRoute;
